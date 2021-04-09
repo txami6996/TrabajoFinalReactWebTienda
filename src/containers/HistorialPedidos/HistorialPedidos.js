ea094923c098952b705a2054a4f7778bf4fbf920
jose sanchez <txami6996@gmail.com>
2021-04-09 20:45:07 +0200

    Version2

--- src/containers/HistorialPedidos/HistorialPedidos.js
+++ src/containers/HistorialPedidos/HistorialPedidos.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import axios from 'axios';
 import Table from 'react-bootstrap/Table';
-import { Button } from "react-bootstrap";
+import axios from 'axios';
+import { Modal, Button } from "react-bootstrap";
 import BotonModal from './BtModalProd';
 export const ContextoAutenticado = React.createContext({
     autenticado: false,
@@ -12,7 +12,8 @@ class historialPedidos extends React.Component {
         this.state = {
             DatosUsuario: [],
             productos: [],
-            error: false
+            error: false,
+            isOpen: false
 
         }
     }
@@ -44,25 +45,26 @@ class historialPedidos extends React.Component {
             });
     }
     borraArticulo = (id, idb) => {
-       // console.log(idb)
+        // console.log(idb)
         axios.delete('https://miprimerproyecto-5055f-default-rtdb.europe-west1.firebasedatabase.app/Pedidos/' + idb + '.json?auth=' + this.props.idToken)
             .then(response => {
                 console.log(response);
             });
         let DatosUsuario = [...this.state.DatosUsuario];
         DatosUsuario.splice(id, 1);
-        this.setState({ DatosUsuario: DatosUsuario });
+        this.setState({ DatosUsuario: DatosUsuario,isOpen:false });
         console.log("borrado")
     }
-
+    openModal = () => this.setState({ isOpen: true });
+    closeModal = () => this.setState({ isOpen: false });
     render() {
         let listaBaseDatos = null;
         listaBaseDatos = (
             <>
                 {this.state.DatosUsuario.map((datos, id) => {
-                   // console.log("estoy en table")
+                    // console.log("estoy en table")
 
-                   // console.log(datos)
+                    // console.log(datos)
                     return (
 
                         <tr key={id}>
@@ -73,7 +75,22 @@ class historialPedidos extends React.Component {
                             <td>
                                 <BotonModal producto={datos.pedidos} />
                             </td>
-                            <td> <Button variant="danger" onClick={() => this.borraArticulo(id, datos.idb)}> X </Button></td>
+                            <td> <Button variant="danger" onClick={this.openModal} > X </Button></td>
+                            <Modal show={this.state.isOpen} onHide={this.resetEstado}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Borrar Pedido</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body> Esta apunto de cancelar un pedido, pulse continuar para confirmar </Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={this.closeModal} >
+                                        cancelar
+                                     </Button>
+                                     <Button variant="secondary" onClick={() => this.borraArticulo(id, datos.idb)} >
+                                        Continuar
+                                     </Button>
+                                     
+                                </Modal.Footer>
+                            </Modal>
                         </tr>
 
                     )
@@ -82,7 +99,7 @@ class historialPedidos extends React.Component {
         )
         return (
             <>
-                <div style={{textAlign:"center"}}><h1>Historial de pedidos</h1></div>
+                <div style={{ textAlign: "center" }}><h1>Historial de pedidos</h1></div>
                 <ContextoAutenticado.Provider
                     value={{
                         autenticado: this.state.autenticado,
